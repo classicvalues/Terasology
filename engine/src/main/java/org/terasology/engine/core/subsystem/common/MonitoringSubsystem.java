374bf108f8d405104594e4004eaea9c936a2cf56
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-11-13 14:44:36 -0800

    doc(MonitoringSubsystem): document micrometer-related methods

--- engine/src/main/java/org/terasology/engine/core/subsystem/common/MonitoringSubsystem.java
+++ engine/src/main/java/org/terasology/engine/core/subsystem/common/MonitoringSubsystem.java
@@ -24,6 +24,8 @@ import java.time.Duration;
 
 public class MonitoringSubsystem implements EngineSubsystem {
 
+    public static final Duration JMX_INTERVAL = Duration.ofSeconds(5);
+
     private AdvancedMonitor advancedMonitor;
 
     @Override
@@ -38,43 +40,67 @@ public class MonitoringSubsystem implements EngineSubsystem {
             advancedMonitor.setVisible(true);
         }
 
-        initMetrics(rootContext.get(Time.class));
+        initMicrometerMetrics(rootContext.get(Time.class));
     }
 
-    private void initMetrics(Time time) {
+    /**
+     * Initialize Micrometer metrics and publishers.
+     *
+     * @see org.terasology.engine.core.GameScheduler GameScheduler for global Reactor metrics
+     * @param time provides statistics
+     *     <p>
+     *     Note {@link org.terasology.engine.core.EngineTime EngineTime}
+     *     does not serve the same role as a {@link Clock micrometer Clock}.
+     *     (Not yet. Maybe it should?)
+     */
+    private void initMicrometerMetrics(Time time) {
+        // Register metrics with the built-in global composite registry.
+        // This makes them available to any agent(s) we add to it.
         MeterRegistry meterRegistry = Metrics.globalRegistry;
-        // Install all the micrometer built-in metrics on the global micrometer registry.
-        new ClassLoaderMetrics().bindTo(meterRegistry);
-        new JvmMemoryMetrics().bindTo(meterRegistry);
-        new JvmGcMetrics().bindTo(meterRegistry);
-        new JvmThreadMetrics().bindTo(meterRegistry);
-        new ProcessorMetrics().bindTo(meterRegistry);
 
-        // Add one of our own.
         Gauge.builder("terasology.fps", time::getFps)
                 .description("framerate")
-                .baseUnit("frames/sec")
+                .baseUnit("Hz")
                 .register(meterRegistry);
 
-        // Make global registry available via JMX.
+        // Publish the global metrics registry on a JMX server.
         MeterRegistry jmxMeterRegistry = new JmxMeterRegistry(new JmxConfig() {
             @Override
             public String get(String key) {
-                return null;
+                return null;  // only needed if we have runtime configuration changes
             }
 
             @Override
             public Duration step() {
-                return Duration.ofSeconds(5);  // default was 1 minute
+                return JMX_INTERVAL;  // default was 1 minute
             }
         }, Clock.SYSTEM);
         Metrics.addRegistry(jmxMeterRegistry);
-
 
         // If we want to make global metrics available to our custom view,
         // we add our custom registry to the global composite:
         //
         // Metrics.addRegistry(DebugOverlay.meterRegistry);
+        //
+        // If we want to see JVM metrics there as well:
+        //
+        // initAllJvmMetrics(DebugOverlay.meterRegistry);
+    }
+
+    /**
+     * Installs all the micrometer built-in metrics.
+     * <p>
+     * Don't add these if you only intend to view them through a JMX registry, because they are
+     * already available from the default JMX server even without micrometer. Add them if you
+     * have a different agent you want them published through.
+     */
+    @SuppressWarnings("unused")
+    void initAllJvmMetrics(MeterRegistry registry) {
+        new ClassLoaderMetrics().bindTo(registry);
+        new JvmMemoryMetrics().bindTo(registry);
+        new JvmGcMetrics().bindTo(registry);
+        new JvmThreadMetrics().bindTo(registry);
+        new ProcessorMetrics().bindTo(registry);
     }
 
     @Override
